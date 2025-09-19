ffc811d00d1bd8c98c0b2b46487c0ff7e22eb397
Happy Kumari <137107490+Happy0936@users.noreply.github.com>
2025-09-19 23:43:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Min Add to Make Parentheses Valid/min-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int minParentheses(string s) {
+        int open = 0, add = 0;
+        for (char c : s) {
+            if (c == '(') {
+                open++;
+            } else { 
+                if (open > 0) {
+                    open--;  
+                } else {
+                    add++;   
+                }
+            }
+        }
+        return add + open;  
+    }
+};
