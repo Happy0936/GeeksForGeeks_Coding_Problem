fed52d470125b77f17add39dcd787773a2828fcb
Happy Kumari <137107490+Happy0936@users.noreply.github.com>
2025-10-03 23:58:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Possible Words From Phone Digits/possible-words-from-phone-digits.cpp
@@ -0,0 +1,43 @@
+class Solution {
+  public:
+    vector<string> possibleWords(vector<int> &arr) {
+        // code here
+            vector<string> mapping = {
+            "",     // 0
+            "",     // 1
+            "abc",  // 2
+            "def",  // 3
+            "ghi",  // 4
+            "jkl",  // 5
+            "mno",  // 6
+            "pqrs", // 7
+            "tuv",  // 8
+            "wxyz"  // 9
+        };
+
+        vector<string> result;
+        string current;
+        backtrack(arr, 0, current, result, mapping);
+        return result;
+    }
+
+private:
+    void backtrack(vector<int>& arr, int idx, string &current,
+                   vector<string> &result, vector<string> &mapping) {
+        if (idx == arr.size()) {
+            if (!current.empty()) result.push_back(current); 
+            return;
+        }
+
+        string letters = mapping[arr[idx]];
+        if (letters.empty()) {
+            backtrack(arr, idx + 1, current, result, mapping);
+        } else {
+            for (char ch : letters) {
+                current.push_back(ch);
+                backtrack(arr, idx + 1, current, result, mapping);
+                current.pop_back();
+            }
+        }
+    }
+};
